718505a61b41f762b857836291cfe76e135a0e31
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-04-13 11:46:59 +0200

    initial public commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # like-a-twitter-rockstar
-because you deserve to feel like the special snowflake you are
+
+This tiny UserScript bumps your Twitter Followers count to make you feel better
+
+...because you deserve to feel like the special snowflake you are ❄️
+
+<img height="250" alt="screenshot of the feature, my twitter count is now 120k" src="https://user-images.githubusercontent.com/711311/38728361-fc2395da-3f0f-11e8-9870-52ceced018a0.png">
+
+### install
+
+* download `index.user.js`
+* open Chrome
+* go to [`chrome://extensions/`](chrome://extensions/)
+* drag and drop `index.user.js`
+* enjoy 🍸
---
+++ index.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name like-a-twitter-rockstar
+// @description Small UserScript that bumps your Twitter Followers count to make you feel better.
+// @version 0.1.0
+// @match https://twitter.com/*
+// ==/UserScript==
+document.head.insertAdjacentHTML('beforeend', `
+  <style>
+    .DashboardProfileCard-content [data-element-term="follower_stats"] [data-count],
+    .ProfilePage-editingOverlay + .AppContainer .ProfileNav-item--followers [data-count] {
+      position: relative !important;
+      text-indent: -100000px !important;
+    }
+
+    .DashboardProfileCard-content [data-element-term="follower_stats"] [data-count]:after,
+    .ProfilePage-editingOverlay + .AppContainer .ProfileNav-item--followers [data-count]:after {
+      content: '120K' !important;
+      position: absolute !important;
+      left: 0 !important;
+      width: 100% !important;
+      text-indent: 0 !important;
+    }
+  </style>
+`);
