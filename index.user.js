9b02935f540e164b0bba02487477b0b8afcf1fe6
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-12-21 09:22:37 +0100

    Revert "Hide likes and favorites count"
    This reverts commit 991c0e6acb39a8b44c8581ba33c87d172f810a11.

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name like-a-twitter-rockstar
 // @description Small UserScript that bumps your Twitter Followers count to make you feel better.
-// @version 0.2.0
+// @version 0.3.0
 // @match https://twitter.com/*
 // ==/UserScript==
 document.head.insertAdjacentHTML('beforeend', `
@@ -19,14 +19,6 @@ document.head.insertAdjacentHTML('beforeend', `
       left: 0 !important;
       width: 100% !important;
       text-indent: 0 !important;
-    }
-    .ProfileTweet-action--retweet .ProfileTweet-actionCount,
-    .ProfileTweet-action--favorite .ProfileTweet-actionCount {
-      visibility: hidden !important;
-    }
-    .tweet-stats-container {
-      height: 0 !important;
-      overflow: hidden !important;
     }
   </style>
 `);
